a9acf9f3ddcb0e278f7ecb16ef86bfedc73be5fd
scw <894565978@qq.com>
2015-07-23 13:15:27 +0800

    Revert "Revert "1.1""
    This reverts commit 010a4a6dd79f3678b683e3a4233c895130190b30.

---
+++ JavaTest/src/lesson/one/B.java
@@ -0,0 +1,14 @@
+package lesson.one;
+
+public class B extends A{
+     public static void main(String[] args){
+    	 B a=new B();
+	    System.out.println(a.geti());
+	    C c=new C();
+	    System.out.println(c.geti());
+	    System.out.println(i);
+     }
+//     public void get(){
+////    	 System.out.print(i);
+//     }
+}
---
+++ JavaTest/src/lesson/one/C.java
@@ -0,0 +1,5 @@
+package lesson.one;
+
+public class C extends A{
+
+}
